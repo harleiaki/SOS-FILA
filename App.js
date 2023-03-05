1e74f7b6c8cc70db0738893033c4270b3d18b14b
Harlei Akira Ohta <96266332+harleiaki@users.noreply.github.com>
2023-03-05 13:40:06 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { View, Image } from 'react-native';
+import { Button, Input, Text } from 'react-native-elements';
+import styles from './styles/MainStyle';
+
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Login from './screens/Login';
+import Principal from './screens/Principal';
+import Card1 from './screens/Card1';
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Principal" component={Principal} /> 
+      <Stack.Screen name="Card1" component={Card1} />                  
+      
+    </Stack.Navigator>
+  );
+}
+
+
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
+
+
