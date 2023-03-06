8355ce6430da1590aa2c674ece7cb23f8f7cb2f1
Harlei Akira Ohta <96266332+harleiaki@users.noreply.github.com>
2023-03-06 11:54:26 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ function MyStack() {
     <Stack.Navigator>
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Principal" component={Principal} /> 
-      <Stack.Screen name="Card1" component={Card1} />                  
+      <Stack.Screen name="Card1" component={Card1} />                        
       
     </Stack.Navigator>
   );
