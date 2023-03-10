d5870b8aaad3b257aef121fb9e5f73aa258e6ed5
Harlei Akira Ohta <96266332+harleiaki@users.noreply.github.com>
2023-03-10 10:17:40 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Image,   StatusBar, SafeAreaView, } from 'react-native';
+import { View, Image, StatusBar, SafeAreaView } from 'react-native';
 import { Button, Input, Text } from 'react-native-elements';
 import styles from './styles/MainStyle';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -8,31 +8,50 @@ import { NavigationContainer } from '@react-navigation/native';
 import Login from './screens/Login';
 import Principal from './screens/Principal';
 import Card1 from './screens/Card1';
+import Card2 from './screens/Card2';
+import Card3 from './screens/Card3';
 import Feed1 from './screens/Feed1';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import Cadastro from './screens/Cadastro';
 
-const Stack = createStackNavigator()
-const Tab = createBottomTabNavigator()
-
-function Tabs() {
-  return (
-      <Tab.Navigator>
-      <Tab.Screen name="Principal" component={Principal}/>    
-      <Tab.Screen name="Feed1" component={Feed1} options={{ title: 'Hospital Feed' }}  />         
-    </Tab.Navigator>
-  )
-}
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+
+  function Tabs() {
+    return (
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="Principal"  component={Principal}  options={{
+          tabBarLabel: 'Principal',
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="hospital-symbol" size={24} color="black" />
+          ),
+        }}
+      />
+        <Tab.Screen name="Feed1" component={Feed1} options={{ 
+                    tabBarLabel: 'Feed',
+                    tabBarIcon: ({ color, size }) => (
+                      <FontAwesome name="hospital-o" size={24} color="black" />
+                    ),
+         }} />
+      </Tab.Navigator>
+    );
+  }
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-      <Stack.Screen name="Login" component={Login} options={{ title: 'Login' }} />    
-      <Stack.Screen name="Principal" component={Tabs} options={{ title: 'Principal' }} />    
-      <Stack.Screen name="Card1" component={Card1} options={{ title: 'Hospital São Pedro' }} />            
-      <Stack.Screen name="Feed1" component={Feed1} options={{ title: 'Hospital Feed' }}  />   
+      <Stack.Navigator screenOptions={{ headerShown: true }}>
+        <Stack.Screen name="Login" component={Login} options={{ title: 'Login' }} />
+        <Stack.Screen name="Principal" component={Tabs} options={{ title: 'Principal' }} />
+
+        <Stack.Screen name="Cadastro" component={Cadastro} options={{ title: 'Cadastro' }} />
+
+        <Stack.Screen name="Feed1" component={Tabs} options={{ title: 'Hospital Feed' }} />
+        <Stack.Screen name="Card1" component={Card1} options={{ title: 'Hospital São Pedro' }} />
+        <Stack.Screen name="Card2" component={Card2} options={{ title: 'Hospital Caridade' }} />
+        <Stack.Screen name="Card3" component={Card3} options={{ title: 'UPA Saúde' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-
--- screens/Feed1.js
+++ screens/Feed1.js
@@ -4,7 +4,6 @@ import {
   ScrollView,
   StyleSheet,
   Image,
-  TouchableOpacity,
 } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons'; //icone relogio,coracao
@@ -15,102 +14,77 @@ const Feed1 = ({ navigation, route }) => {
   return (
     <ScrollView>
       <Card>
-      <Text style={{ flex: 1, flexDirection: 'row', alignItems: 'center', marginTop: 10, marginBottom: 10, fontWeight: 'bold' }}>
-        <Image style={styles.image}
-          source={require('../logo/joel.png')}
-        />  Joel Miller</Text>
-      <Card.Divider />
-      <View style={styles.container}>
-        <View>
-          <TouchableOpacity
-            style={styles.back}
-            onPress={() => navigation.navigate('Card1')}>
-            <Image
-              style={styles.logo}
-              source={require('../assets/icon.png')}
-            />
+        <Text style={{ flex: 1, flexDirection: 'row', alignItems: 'center', marginTop: 10, marginBottom: 10, fontWeight: 'bold' }}>
+          <Image style={styles.image}
+            source={require('../logo/joel.png')}
+          />  Joel Miller</Text>
+        <Card.Divider />
+        <View style={styles.container}>
+          <View>
+              <Image
+                style={styles.logo}
+                source={require('../logo/Estacionamento.jpg')}
+              />
+              <View></View>
+          </View>
+
+          <View>
+              <Image
+                style={styles.logo}
+                source={require('../logo/Recepcao.jpg')}
+              />
             <View></View>
-          </TouchableOpacity>
-        </View>
+          </View>
 
-        <View>
-          <TouchableOpacity
-            style={styles.back}
-            onPress={() => navigation.navigate('Card1')}>
-            <Image
-              style={styles.logo}
-              source={require('../assets/icon.png')}
-            />
-          </TouchableOpacity>
-          <View></View>
+          <View>
+              <Image
+                style={styles.logo}
+                source={require('../logo/Vazio.jpg')}
+              />
+            <View></View>
+          </View>
         </View>
-
-        <View>
-          <TouchableOpacity 
-          onPress={() => navigation.navigate('Card1')}>
-            <Image
-              style={styles.logo}
-              source={require('../assets/icon.png')}
-            />
-          </TouchableOpacity>
-          <View></View>
-        </View>
-      </View>
-      <Text style={{ marginBottom: 10, fontWeight: 'bold', }}> <Entypo name="location-pin" size={20} color="black" />Hospital das clinicas</Text>
-      <Text style={{ marginBottom: 10 }}>
-        The idea with React Native Elements is more about component
-        structure than actual design.
-      </Text>
-      <Text style={{ marginBottom: 10, fontWeight: 'bold', }}>  <AntDesign name="like2" size={20} color="black" /> 19  <FontAwesome name="commenting" size={24} color="black" /> 4</Text>
+        <Text style={{ marginBottom: 10, fontWeight: 'bold', }}> <Entypo name="location-pin" size={20} color="black" />Hospital das clinicas</Text>
+        <Text style={{ marginBottom: 10 }}>
+          The idea with React Native Elements is more about component
+          structure than actual design.
+        </Text>
+        <Text style={{ marginBottom: 10, fontWeight: 'bold', }}>  <AntDesign name="like2" size={20} color="black" /> 19  <FontAwesome name="commenting" size={24} color="black" /> 4</Text>
       </Card>
       <Card>
-      <Text style={{ flex: 1, flexDirection: 'row', alignItems: 'center', marginTop: 10, marginBottom: 10, fontWeight: 'bold' }}>
-        <Image style={styles.image}
-          source={require('../logo/ellie.png')}
-        />  Ellie Williams</Text>
-      <Card.Divider />
-      <View style={styles.container}>
-        <View>
-          <TouchableOpacity
-            style={styles.back}
-            onPress={() => navigation.navigate('Card1')}>
-            <Image
-              style={styles.logo}
-              source={require('../assets/icon.png')}
-            />
-            <View></View>
-          </TouchableOpacity>
-        </View>
+        <Text style={{ flex: 1, flexDirection: 'row', alignItems: 'center', marginTop: 10, marginBottom: 10, fontWeight: 'bold' }}>
+          <Image style={styles.image}
+            source={require('../logo/ellie.png')}
+          />  Ellie Williams</Text>
+        <Card.Divider />
+        <View style={styles.container}>
+          <View>
+              <Image
+                style={styles.logo}
+                source={require('../logo/Lanchonete.jpg')}
+              />
+          </View>
 
-        <View>
-          <TouchableOpacity
-            style={styles.back}
-            onPress={() => navigation.navigate('Card1')}>
-            <Image
-              style={styles.logo}
-              source={require('../assets/icon.png')}
-            />
-          </TouchableOpacity>
-          <View></View>
-        </View>
+          <View>
+              <Image
+                style={styles.logo}
+                source={require('../logo/Jardim.jpg')}
+              />
+          </View>
 
-        <View>
-          <TouchableOpacity 
-          onPress={() => navigation.navigate('Card1')}>
-            <Image
-              style={styles.logo}
-              source={require('../assets/icon.png')}
-            />
-          </TouchableOpacity>
-          <View></View>
+          <View>
+              <Image
+                style={styles.logo}
+                source={require('../logo/Kids.jpg')}
+              />
+          </View>
         </View>
-      </View>
-      <Text style={{ marginBottom: 10, fontWeight: 'bold', }}> <Entypo name="location-pin" size={20} color="black" />Hospital da Criança</Text>
-      <Text style={{ marginBottom: 10 }}>
-        The idea with React Native Elements is more about component
-        structure than actual design.
-      </Text>
-      <Text style={{ marginBottom: 10, fontWeight: 'bold', }}>  <AntDesign name="like2" size={20} color="black" /> 15  <FontAwesome name="commenting" size={24} color="black" /> 3</Text>
+        <Text style={{ marginBottom: 10, fontWeight: 'bold', }}> <Entypo name="location-pin" size={20} color="black" />Hospital da Criança</Text>
+        <Text style={{ marginBottom: 10 }}>
+          The idea with React Native Elements is more about component
+          structure than actual design.
+        </Text>
+        <Text style={{ marginBottom: 10, fontWeight: 'bold', }}>  <AntDesign name="like2" size={20} color="black" /> 15  <FontAwesome name="commenting" size={24} color="black" /> 3</Text>
       </Card>
     </ScrollView>
   );
@@ -126,16 +100,17 @@ const styles = StyleSheet.create({
     shadowRadius: 4,
   },
   logo: {
-    marginRight:10,
+    marginRight: 10,
     height: 80,
     width: 100,
+
   },
   image: {
     height: 33,
     width: 31,
     marginBottom: -10,
     marginLeft: 10,
-  }
+  },
 },
 );
 
