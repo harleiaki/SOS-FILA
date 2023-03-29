dbe5f3a920fd47038b99c68c4d578b699923abfd
Harlei Akira Ohta <96266332+harleiaki@users.noreply.github.com>
2023-03-29 11:53:46 -0300

    Update Feed3.js
    Signed-off-by: Harlei Akira Ohta <96266332+harleiaki@users.noreply.github.com>

--- screens/Feed3.js
+++ screens/Feed3.js
@@ -16,7 +16,7 @@ const Feed1 = ({ navigation, route }) => {
             <Card>
                 <Text style={{ flex: 1, flexDirection: 'row', alignItems: 'center', marginTop: 10, marginBottom: 10, fontWeight: 'bold' }}>
                     <Image style={styles.image}
-                        source={require('../logo/pessoas/WilsonSmith.png')} />  WilsonSmith </Text>
+                        source={require('../logo/pessoas/WilsonSmith.png')} />  Wilson Smith </Text>
                 <Card.Divider />
                 <View style={styles.container}>
                     <View>
